d5a7378c568632d32f77abc265a82add5a4825e9
anthonytazouti <78940746+anthonytazouti@users.noreply.github.com>
2022-12-27 15:04:35 -0600

    Update Print_Statements_and_Arithmetic.py

---
+++ Print_Statements_and_Arithmetic.py
@@ -0,0 +1,24 @@
+#1
+print("Loyal sons forever true, ")
+print("And we will fight the battle through.")
+print("And when we hit that line we will hit it hard")
+print("every yard for I.S.U.")
+#2
+print(39 + 3)
+print(39 + 3)
+
+
+trainTicket = 13
+distanceInmiles = 45
+mpg = 15
+pricePergallon = 4
+perMilemtnc = .05
+
+priceTodrive = (((perMilemtnc * distanceInmiles)) + (distanceInmiles / mpg) * pricePergallon)
+
+print (priceTodrive)
+print (trainTicket, "dollars is a cheaper price to ride the train.")
+
+#5
+pies = (5+2)
+print(pies)
